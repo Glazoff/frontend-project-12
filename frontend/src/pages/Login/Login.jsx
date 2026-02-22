011d54a054b22b76796f235d360575f0a5ca8c82
Glazoff <glazov_evgen@mail.ru>
2026-02-22 16:46:32 +0300

    step_2/add_form_to_login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,4 +1,20 @@
+import { Formik, Field, Form } from 'formik';
+
+const initialValues = { name: "", password: "" }
+
 export function Login() {
 
-  return <dev>Login</dev>
+  return(
+    <Formik initialValues={initialValues}>
+      <Form>
+        <label htmlFor="name">Name</label>
+        <Field type="name" name="name"></Field>
+
+        <label htmlFor="password">Password</label>
+        <Field type="password" name="password"></Field>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  )
 }
