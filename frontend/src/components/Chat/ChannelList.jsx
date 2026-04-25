adce7d465acb53faaaf9d89ab223cc440350b242
Glazoff <glazov_evgen@mail.ru>
2026-04-25 17:06:34 +0300

    fix_import

--- frontend/src/components/Chat/ChannelList.jsx
+++ frontend/src/components/Chat/ChannelList.jsx
@@ -7,6 +7,7 @@ import { setCurrentChannelId, removeChannel } from '../../store/channelsSlice';
 import { RemoveChannelModal } from './RemoveChannelModal';
 import { removeChannel as removeChannelApi } from '../../api/channels';
 import { useToastNotifications } from '../ToastNotification';
+import { ChannelMenu } from '../Chat/ChannelMenu';
 
 export function ChannelList() {
   const { t } = useTranslation();
